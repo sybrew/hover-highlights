740a2e1302e2f14a9d09185c6c66d198fb818454
Sybre Waaijer <sybrew@users.noreply.github.com>
2023-04-03 07:36:07 +0200

    Written by ChatGPT.

---
+++ hover-highlights.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: Hover Highlights - Editor highlighting
+ * Plugin URI: https://github.com/sybrew/hover-highlights
+ * Description: Adds custom CSS to the Gutenberg editor to highlight blocks on hover and focus.
+ * Version: 1.0.0
+ * Author: Sybre Waaijer
+ * Author URI: https://cyberwire.nl/
+ * License: GPLv3
+ * License URI: https://www.gnu.org/licenses/gpl-3.0.html
+ */
+
+function hover_highlights_output_focus_style() {
+    echo <<<'CSS'
+<style>
+    .wp-block:hover:not(:has(:focus),:focus) {
+        outline: 1px dashed #1e1e1e;
+        box-shadow: 0 0 2px #e1e1e1;
+    }
+</style>
+CSS;
+}
+add_action( 'enqueue_block_editor_assets', 'hover_highlights_output_focus_style' );
